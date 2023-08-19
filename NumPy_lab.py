edc7d352ef4702d5b5a55638ca8f908635d89e12
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-19 16:00:34 +0700

    add/delete row and column

--- NumPy_lab.py
+++ NumPy_lab.py
@@ -1,13 +1,26 @@
-import numpy as np
+import pandas as pd
 
-# Create a 3D array
-arr_3d = np.array([[[1, 2, 3],
-                    [4, 5, 6]],
-                   [[7, 8, 9],
-                    [10, 11, 12]],
-                   [[13, 14, 15],
-                    [16, 17, 18]]])
+# Create a sample DataFrame
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 22]}
+df = pd.DataFrame(data)
 
-# Split into two new arrays along the first axis
-split_arrs = np.array_split(arr_3d, 2, axis=0)
-print(split_arrs)
+# Adding a new row by index
+new_row = pd.Series({'Name': 'David', 'Age': 28})
+df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
+
+# Deleting a row by index
+df.drop(1, inplace=True)  # Delete the row with index 1
+
+# Adding a new row by key (dictionary)
+new_row_dict = {'Name': 'Eve', 'Age': 29}
+new_row_df = pd.DataFrame(new_row_dict, index=[len(df)])  # Create a DataFrame for the new row
+df = pd.concat([df, new_row_df], ignore_index=True)  # Concatenate the original DataFrame and the new row DataFrame
+
+# Deleting a row by key (condition)
+df = df[df['Name'] != 'Charlie']  # Delete rows where the 'Name' is 'Charlie'
+
+print(df)
+
+
+
