630a6ebbb15f3ef39a407e2ec97ac80b07f133c8
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-20 13:40:58 +0700

    DataFrame with Duplicates Removed

--- NumPy_lab.py
+++ NumPy_lab.py
@@ -1,13 +1,9 @@
 import pandas as pd
 
-data = {'A' : [1,2,3],
-        'B' : [4,5,6],
-        'C' : [7,8,9]}
+data = {'A' : [1,2,2,3,4,4],
+        'B' : ['apple', 'banana', 'banana', 'orange', 'apple', 'apple']}
 
 df = pd.DataFrame(data)
 
-column_sums = df.sum(axis=0)
-print("Column : ", column_sums)
-
-row_sums = df.sum(axis=1)
-print("Row Sums: ", row_sums)
+df_no_duplicates = df.drop_duplicates()
+print("DataFrame with Duplicates Removed: ", df_no_duplicates)
