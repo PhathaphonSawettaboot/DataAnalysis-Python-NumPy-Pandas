cae3a90e45b4102e77edecba845a7559731c5ba3
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-22 15:30:26 +0700

    Update NumPy_lab.py

--- NumPy_lab.py
+++ NumPy_lab.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
-data = {'A' : [1,2,2,3,4,4],
-        'B' : ['apple', 'banana', 'banana', 'orange', 'apple', 'apple']}
+# Sample data
+employees_data = {'Employee_ID': [101, 102, 103, 104],
+                  'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+                  'Department': ['HR', 'Finance', 'IT', 'Marketing']}
+salaries_data = {'Employee_ID': [102, 103, 105, 106],
+                 'Salary': [50000, 60000, 55000, 62000]}
 
-df = pd.DataFrame(data)
+employees = pd.DataFrame(employees_data)
+salaries = pd.DataFrame(salaries_data)
 
-df_no_duplicates = df.drop_duplicates()
-print("DataFrame with Duplicates Removed: ", df_no_duplicates)
+inner_merge = pd.merge(employees, salaries, on = 'Employee_ID', how = 'inner')
+print(inner_merge)
+
+inner_merge = pd.merge(employees, salaries, on = 'Employee_ID', how = 'outer')
+print(inner_merge)
+
+inner_merge = pd.merge(employees, salaries, on = 'Employee_ID', how = 'right')
+print(inner_merge)
