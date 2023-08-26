23f35f0943238754ed504af98f79d25d2a4a23bd
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-26 13:16:55 +0700

    correlation heatmap

--- NumPy_lab.py
+++ NumPy_lab.py
@@ -1,9 +1,21 @@
-import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-sizes = [15, 30, 45, 10]
+# Create a sample DataFrame
+data = {
+    'A': [1, 2, 3, 4, 5],
+    'B': [5, 4, 3, 2, 1],
+    'C': [3, 3, 3, 3, 3]
+}
+df = pd.DataFrame(data)
 
-plt.boxplot(sizes)
-plt.ylabel('Value')
+# Calculate the correlation matrix
+correlation_matrix = df.corr()
+
+# Create a heatmap using Seaborn
+plt.figure(figsize=(8, 6))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', center=0)
+plt.title('Correlation Heatmap')
 plt.grid(True)
 plt.show()
