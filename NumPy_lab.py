21ae8c4e66fa6da1ac2c2cd882bc4679f1a9b56f
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-31 11:16:41 +0700

    polynomial regression

--- NumPy_lab.py
+++ NumPy_lab.py
@@ -1,17 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
-X = np.array([[1],[2],[3],[4],[5]])
-y = np.array([2,3.4,5,6.5,8])
+# Sample data
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([1, 4, 9, 16, 25])
 
+# Transform features to higher degrees
+poly = PolynomialFeatures(degree=2)
+X_poly = poly.fit_transform(X)
+
+# Create and fit the model
 model = LinearRegression()
-model.fit(X,y)
+model.fit(X_poly, y)
 
-predictions = model.predict(X)
+# Make predictions
+X_range = np.linspace(0, 6, 100).reshape(-1, 1)
+X_range_poly = poly.transform(X_range)
+predictions = model.predict(X_range_poly)
 
-plt.scatter(X,y, label='Data')
-plt.plot(X, predictions, color='red', label='Linear Regression')
+# Plot the data and polynomial regression curve
+plt.scatter(X, y, label='Data')
+plt.plot(X_range, predictions, color='red', label='Polynomial Regression')
 plt.xlabel('X')
 plt.ylabel('y')
 plt.legend()
