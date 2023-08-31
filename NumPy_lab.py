296b409ebd39758e494c0d0b156cc475fda972dc
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-31 10:51:19 +0700

    heatmap

--- NumPy_lab.py
+++ NumPy_lab.py
@@ -1,12 +1,21 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Creating a DataFrame with duplicates
-data = {'Name': ['Alice', 'Bob', 'Alice', 'David'],
-        'Age': [25, 30, 25, 22]}
+# Create a sample DataFrame
+data = {
+    'Age': [25, 30, 35, 40, 45],
+    'Income': [50000, 60000, 75000, 90000, 100000],
+    'Savings': [10000, 15000, 20000, 25000, 30000],
+    'Spending': [3000, 4000, 4500, 5000, 5500]
+}
 df = pd.DataFrame(data)
 
-# Check for duplicates
-print(df.duplicated())
+# Calculate the correlation matrix
+correlation_matrix = df.corr()
 
-# Drop duplicates
-df.drop_duplicates(inplace=True)
+# Create the heatmap
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm")
+plt.title("Correlation Matrix Heatmap")
+plt.show()
